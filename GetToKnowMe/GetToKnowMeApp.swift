d79fed0c697c36883019dccf5382563c6849d4c1
Van Truong <van.t414@gmail.com>
2023-10-15 14:58:30 -0700

    Initial Commit

---
+++ GetToKnowMe/GetToKnowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetToKnowMeApp.swift
+//  GetToKnowMe
+//
+//  Created by Van Truong on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GetToKnowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
